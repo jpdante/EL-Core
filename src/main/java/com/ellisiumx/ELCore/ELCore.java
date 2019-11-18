d70aa0685d6ba70a876525c0e19f3abb176906d3
João Pedro <joaopdante@gmail.com>
2019-11-18 04:39:38 -0300

    Create project

---
+++ src/main/java/com/ellisiumx/ELCore/ELCore.java
@@ -0,0 +1,19 @@
+package com.ellisiumx.ELCore;
+
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ELCore extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+    }
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}

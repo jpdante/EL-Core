fbc86ab37434e05b72bc1a0044d7984ca5ff33d1
João Pedro <joaopdante@gmail.com>
2019-11-18 22:07:14 -0300

    Update LanguageDB.java

--- src/main/java/com/ellisiumx/ELCore/Lang/LanguageDB.java
+++ src/main/java/com/ellisiumx/ELCore/Lang/LanguageDB.java
@@ -1,19 +1,47 @@
 package com.ellisiumx.ELCore.Lang;
 
+import java.util.HashMap;
+
 public class LanguageDB {
 
     public final String Locale;
+    public final String Language;
+    private HashMap<String, String> translation;
 
     public LanguageDB() {
         Locale = "en_US";
+        Language = "English";
+        translation = new HashMap<String, String>();
     }
 
-    public LanguageDB(String locale) {
+    public LanguageDB(String locale, String language) {
         Locale = locale;
+        Language = language;
+        translation = new HashMap<String, String>();
     }
 
-    public String getTranslation(String msgKey) {
-        return "";
+    public boolean insertTranslation(String key, String value) {
+        if(this.translation.containsKey(key)) return false;
+        this.translation.put(key, value);
+        return true;
     }
 
+    public boolean removeTranslation(String key) {
+        if(!this.translation.containsKey(key)) return false;
+        this.translation.remove(key);
+        return true;
+    }
+
+    public String getTranslation(String key) {
+        if(!this.translation.containsKey(key)) return "§cmissing var " + key + "§f";
+        return this.translation.get(key);
+    }
+
+    public String[] getKeys() {
+        return (String[]) this.translation.keySet().toArray();
+    }
+
+    public String[] getValues() {
+        return (String[]) this.translation.values().toArray();
+    }
 }
